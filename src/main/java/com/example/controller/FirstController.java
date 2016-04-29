366baa877b56bec91ec43def2e143336d8e21b0e
Deniz Silahcılar <deniz.silahcilar@PT0001.local>
2016-04-29 10:54:11 +0300

    initial commit

---
+++ src/main/java/com/example/controller/FirstController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class FirstController {
+
+    @RequestMapping(path = "/first")
+    public String first() {
+        return "First controler";
+    }
+
+}
